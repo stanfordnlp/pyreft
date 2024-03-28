bcdbaf7fe2e7d65e00583c92e5c05ff831fe374e
frankaging <zxw0424@gmail.com>
2024-03-27 21:57:50 -0700

    commit file

---
+++ pyreft/interventions.py
@@ -0,0 +1,59 @@
+import torch
+from pyvene import (
+    ConstantSourceIntervention,
+    TrainableIntervention,
+    DistributedRepresentationIntervention,
+)
+from pyvene.models.layers import LowRankRotateLayer
+from transformers.activations import ACT2FN
+
+
+class ConditionedSourceLowRankRotatedSpaceIntervention(
+    ConstantSourceIntervention,
+    TrainableIntervention, 
+    DistributedRepresentationIntervention
+):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, keep_last_dim=True)
+        rotate_layer = LowRankRotateLayer(self.embed_dim, kwargs["low_rank_dimension"])
+        self.rotate_layer = torch.nn.utils.parametrizations.orthogonal(rotate_layer)
+        self.learned_source = torch.nn.Linear(
+            self.embed_dim, kwargs["low_rank_dimension"]).to(
+            kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
+        self.dropout = torch.nn.Dropout(kwargs["dropout"] if "dropout" in kwargs else 0.0)
+        self.act_fn = ACT2FN["linear"] if "act_fn" not in kwargs or kwargs["act_fn"] is None else ACT2FN[kwargs["act_fn"]]
+        
+    def forward(
+        self, base, source=None, subspaces=None
+    ):
+        rotated_base = self.rotate_layer(base)
+        output = base + torch.matmul(
+            (self.act_fn(self.learned_source(base)) - rotated_base), self.rotate_layer.weight.T
+        )
+        return self.dropout(output.to(base.dtype))
+
+
+class ConditionedSourceLowRankIntervention(
+    ConstantSourceIntervention,
+    TrainableIntervention, 
+    DistributedRepresentationIntervention
+):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, keep_last_dim=True)
+        self.proj_layer = torch.nn.Linear(
+            self.embed_dim, kwargs["low_rank_dimension"], bias=kwargs["add_bias"]).to(
+            kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
+        self.learned_source = torch.nn.Linear(
+            self.embed_dim, kwargs["low_rank_dimension"]).to(
+            kwargs["dtype"] if "dtype" in kwargs else torch.bfloat16)
+        self.dropout = torch.nn.Dropout(kwargs["dropout"] if "dropout" in kwargs else 0.0)
+        self.act_fn = ACT2FN["linear"] if "act_fn" not in kwargs or kwargs["act_fn"] is None else ACT2FN[kwargs["act_fn"]]
+        
+    def forward(
+        self, base, source=None, subspaces=None
+    ):
+        proj_base = self.proj_layer(base)
+        output = base + torch.matmul(
+            (self.act_fn(self.learned_source(base)) - proj_base), self.proj_layer.weight
+        )
+        return self.dropout(output.to(base.dtype))
