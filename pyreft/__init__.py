869884bd2cd6a021c0341007d26c22b474248578
frankaging <zxw0424@gmail.com>
2024-03-29 22:11:50 -0700

    adding chat demo

--- pyreft/__init__.py
+++ pyreft/__init__.py
@@ -2,6 +2,11 @@
 from .utils import TaskType, get_reft_model
 from .config import ReftConfig
 
+# models
+from .reft_model import (
+    ReftModel
+)
+
 # trainers
 from .reft_trainer import (
     ReftTrainerForCausalLM,
@@ -20,5 +25,6 @@ from .dataset import (
     ReftDataCollator,
     ReftDataset,
     ReftSupervisedDataset,
-    make_last_position_supervised_data_module
+    make_last_position_supervised_data_module,
+    get_intervention_locations
 )
--- pyreft/dataset.py
+++ pyreft/dataset.py
@@ -48,6 +48,65 @@ from collections import defaultdict
 from transformers import DataCollator
 
 
+def parse_positions(positions: str):
+    # parse position
+    first_n, last_n = 0, 0
+    if "+" in positions:
+        first_n = int(positions.split("+")[0].strip("f"))
+        last_n = int(positions.split("+")[1].strip("l"))
+    else:
+        if "f" in positions:
+            first_n = int(positions.strip("f"))
+        elif "l" in positions:
+            last_n = int(positions.strip("l"))
+    return first_n, last_n
+
+
+def get_intervention_locations(**kwargs):
+    """
+    This function generates the intervention locations.
+
+    For your customized dataset, you want to create your own function.
+    """
+    # parse kwargs
+    share_weights = kwargs["share_weights"] if "share_weights" in kwargs else False
+    last_position = kwargs["last_position"]
+    if "positions" in kwargs:
+        _first_n, _last_n = parse_positions(kwargs["positions"])
+    else:
+        _first_n, _last_n = kwargs["first_n"], kwargs["last_n"]
+    num_interventions = kwargs["num_interventions"]
+    pad_mode = kwargs["pad_mode"] if "pad_mode" in kwargs else "first"
+
+    first_n = min(last_position // 2, _first_n)
+    last_n = min(last_position // 2, _last_n)
+
+    pad_amount = (_first_n - first_n) + (_last_n - last_n)
+    pad_position = -1 if pad_mode == "first" else last_position
+    if share_weights or (first_n == 0 or last_n == 0):
+        position_list = [i for i in range(first_n)] + \
+            [i for i in range(last_position - last_n, last_position)] + \
+            [pad_position for _ in range(pad_amount)]
+        intervention_locations = [position_list]*num_interventions
+    else:
+        left_pad_amount = (_first_n - first_n)
+        right_pad_amount = (_last_n - last_n)
+        left_intervention_locations = [i for i in range(first_n)] + [pad_position for _ in range(left_pad_amount)]
+        right_intervention_locations = [i for i in range(last_position - last_n, last_position)] + \
+            [pad_position for _ in range(right_pad_amount)]
+        # after padding, there could be still length diff, we need to do another check
+        left_len = len(left_intervention_locations)
+        right_len = len(right_intervention_locations)
+        if left_len > right_len:
+            right_intervention_locations += [pad_position for _ in range(left_len-right_len)]
+        else:
+            left_intervention_locations += [pad_position for _ in range(right_len-left_len)]
+        intervention_locations = [left_intervention_locations]*(num_interventions//2) + \
+            [right_intervention_locations]*(num_interventions//2)
+    
+    return intervention_locations
+
+
 @dataclass
 class ReftDataCollator(object):
     """Collate examples for ReFT."""
@@ -64,59 +123,7 @@ class ReftDataCollator(object):
 class ReftDataset(Dataset):
         
     def get_intervention_locations(self, **kwargs):
-        """
-        This function generates the intervention locations.
-
-        For your customized dataset, you want to create your own function.
-        """
-        # parse kwargs
-        share_weights = kwargs["share_weights"]
-        last_position = kwargs["last_position"]
-        _first_n, _last_n = kwargs["first_n"], kwargs["last_n"]
-        num_interventions = kwargs["num_interventions"]
-        pad_mode = kwargs["pad_mode"]
-
-        first_n = min(last_position // 2, _first_n)
-        last_n = min(last_position // 2, _last_n)
-    
-        pad_amount = (_first_n - first_n) + (_last_n - last_n)
-        pad_position = -1 if pad_mode == "first" else last_position
-        if share_weights or (first_n == 0 or last_n == 0):
-            position_list = [i for i in range(first_n)] + \
-                [i for i in range(last_position - last_n, last_position)] + \
-                [pad_position for _ in range(pad_amount)]
-            intervention_locations = [position_list]*num_interventions
-        else:
-            left_pad_amount = (_first_n - first_n)
-            right_pad_amount = (_last_n - last_n)
-            left_intervention_locations = [i for i in range(first_n)] + [pad_position for _ in range(left_pad_amount)]
-            right_intervention_locations = [i for i in range(last_position - last_n, last_position)] + \
-                [pad_position for _ in range(right_pad_amount)]
-            # after padding, there could be still length diff, we need to do another check
-            left_len = len(left_intervention_locations)
-            right_len = len(right_intervention_locations)
-            if left_len > right_len:
-                right_intervention_locations += [pad_position for _ in range(left_len-right_len)]
-            else:
-                left_intervention_locations += [pad_position for _ in range(right_len-left_len)]
-            intervention_locations = [left_intervention_locations]*(num_interventions//2) + \
-                [right_intervention_locations]*(num_interventions//2)
-        
-        return intervention_locations
-
-
-def parse_positions(positions: str):
-    # parse position
-    first_n, last_n = 0, 0
-    if "+" in positions:
-        first_n = int(positions.split("+")[0].strip("f"))
-        last_n = int(positions.split("+")[1].strip("l"))
-    else:
-        if "f" in positions:
-            first_n = int(positions.strip("f"))
-        elif "l" in positions:
-            last_n = int(positions.strip("l"))
-    return first_n, last_n
+        return get_intervention_locations(**kwargs)
 
 
 class ReftSupervisedDataset(ReftDataset):
