7026abf5e06839319048d4f8402f35202bfd7342
Zen <zxw0424@gmail.com>
2024-04-08 20:03:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setup(
     name="pyreft",
-    version="0.0.3",
+    version="0.0.4",
     description="REFT: Representation Finetuning for Language Models",
     long_description=long_description,
     long_description_content_type='text/markdown',
