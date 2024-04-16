3ede1015221a4f4115c4ae98d72e699086411beb
PinetreePantry <peterwz@stanford.edu>
2024-04-16 07:44:55 -0700

    Utils for compositional ReFT experiments

--- pyreft/dataset.py
+++ pyreft/dataset.py
@@ -249,6 +249,109 @@ class ReftDataset(Dataset):
         return result
 
 
+class ReftRawDataset(Dataset):
+
+    def __init__(
+        self, task: str, data_path: str,
+        tokenizer: transformers.PreTrainedTokenizer,
+        data_split="train", dataset=None, seed=42, max_n_example=None, 
+        **kwargs,
+    ):
+        super(ReftRawDataset, self).__init__()
+        result = defaultdict(list)
+
+        if dataset is None:
+            print("loading data for dataset: ", data_path)
+            if data_path.endswith(".json"):
+                task_dataset = load_dataset("json", data_files=data_path)[data_split]
+            else:
+                task_dataset = load_dataset(data_path)[data_split]
+        else:
+            task_dataset = dataset
+        if max_n_example is not None:
+            task_dataset = task_dataset.shuffle(seed=seed)
+            task_dataset = task_dataset.select(range(max_n_example))
+
+        # save raw_dataset pointer for access raw strings
+        self.raw_dataset = task_dataset if data_split != "train" else None
+        first_n, last_n = parse_positions(kwargs["position"])
+        
+        # tokenize and intervene
+        for i, data_item in enumerate(tqdm(task_dataset)):
+            base_prompt = data_item["instruction"]
+            base_input = base_prompt + data_item["output"] + tokenizer.eos_token
+
+            # tokenize
+            base_prompt_ids = tokenizer(
+                base_prompt, max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")["input_ids"][0]
+            base_prompt_length = len(base_prompt_ids)
+            if data_split == "train":
+                base_input_ids = tokenizer(
+                    base_input, max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")["input_ids"][0]
+                output_ids = copy.deepcopy(base_input_ids)
+                output_ids[:base_prompt_length] = IGNORE_INDEX
+                    
+                result["input_ids"].append(base_input_ids)
+                result["labels"].append(output_ids)
+            else:
+                # print("Assuming test split for now")
+                result["input_ids"].append(base_prompt_ids)
+            last_position = base_prompt_length
+                
+            # get intervention locations
+            intervention_locations = self.get_intervention_locations(
+                last_position=last_position, 
+                first_n=first_n, 
+                last_n=last_n,
+                pad_mode="first",
+                **kwargs
+            )
+            result["intervention_locations"].append(intervention_locations)
+            result["id"].append(i)
+            
+            # add a single padding token BEFORE input_ids and fix everything
+            result["input_ids"][-1] = torch.cat((torch.tensor([tokenizer.pad_token_id,]), result["input_ids"][-1]))
+            if data_split == "train":
+                result["labels"][-1] = torch.cat((torch.tensor([IGNORE_INDEX]), result["labels"][-1]))
+            result["intervention_locations"][-1] = (torch.IntTensor(result["intervention_locations"][-1]) + 1).tolist()
+            result["attention_mask"].append((result["input_ids"][-1] != tokenizer.pad_token_id).int())
+            if "subspaces" in data_item:
+                num_interventions = kwargs["num_interventions"]
+                share_weights = kwargs["share_weights"] if "share_weights" in kwargs else False
+                if share_weights:
+                    num_interventions = num_interventions // 2
+                # we now assume each task has a constant subspaces
+                _subspaces = [data_item["subspaces"]] * num_interventions
+                result["subspaces"].append(_subspaces)
+        
+        self.input_ids = result["input_ids"]
+        self.attention_mask = result["attention_mask"]
+        self.intervention_locations = result["intervention_locations"]
+        self.labels = result["labels"] if "labels" in result else None
+        self.subspaces = result["subspaces"] if "subspaces" in result else None
+        self.id = result["id"]
+
+    def get_intervention_locations(self, **kwargs):
+        return get_intervention_locations(**kwargs)
+    
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, i) -> Dict[str, torch.Tensor]:
+        return_dict = dict(
+            input_ids=self.input_ids[i],
+            attention_mask=self.attention_mask[i],
+            intervention_locations=self.intervention_locations[i],
+            id=self.id[i],
+        )
+        if self.labels is not None:
+            return_dict["labels"] = self.labels[i]
+        if self.subspaces is not None:
+            return_dict["subspaces"] = self.subspaces[i]
+        return return_dict
+
+
+
 class ReftClassificationDataset(ReftDataset):
     """
     A ReftClassificationDataset only contains a single text field
