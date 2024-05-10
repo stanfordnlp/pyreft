262e255f83b1e46d195409ca04032226e41c1cb1
Amir Zur <amir.zur@hotmail.com>
2024-05-10 10:41:26 -0700

    fixed intervention type in evaluated_trained.sh script. should eventually make sure that config is simply read from model that is passed in

--- examples/loreft/evaluate_trained.sh
+++ examples/loreft/evaluate_trained.sh
@@ -1,12 +1,14 @@
 export task=$1
 export loaddir=$2
 
+# NOTE: currently must replicate exact config from training script
+
 python train.py -task $task \
 -data_dir dataset \
 -model "meta-llama/Llama-2-7b-chat-hf" \
 -seed 42 \
 -l "18;28" -r 2 -p "f1+l1" -e 1 -lr 9e-4 \
--type LoreftIntervention \
+-type NoreftIntervention \
 -gradient_accumulation_steps 1 \
 -batch_size 16 \
 -eval_batch_size 16 \
