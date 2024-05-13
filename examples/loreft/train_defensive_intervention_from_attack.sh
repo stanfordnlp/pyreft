d697a6265b77a2a1c269ce294ba61def5527afeb
Amir Zur <amir.zur@hotmail.com>
2024-05-12 19:43:44 -0700

    change save directory in training defensive reft model (so that I don't accidentally overwrite things again)

--- examples/loreft/train_defensive_intervention_from_attack.sh
+++ examples/loreft/train_defensive_intervention_from_attack.sh
@@ -8,7 +8,7 @@ python train_defensive_intervention.py \
 --batch_size 10 \
 --learning_rate 4e-3 \
 --num_train_epochs 5.0 \
---output_dir defense_results \
+--output_dir defense_results_2 \
 --logging_steps 1 \
 --positions "f1+l1" \
 --share_weights \
