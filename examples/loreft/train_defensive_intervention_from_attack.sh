5107c90c0bc3b427f387fb04cf503597ab91384d
Amir Zur <amir.zur@hotmail.com>
2024-05-12 18:46:56 -0700

    updated train size in bash script for learning defensive model

--- examples/loreft/train_defensive_intervention_from_attack.sh
+++ examples/loreft/train_defensive_intervention_from_attack.sh
@@ -4,7 +4,7 @@ python train_defensive_intervention.py \
 --model_name_or_path meta-llama/Llama-2-7b-chat-hf \
 --layers "18;28" \
 --low_rank_dimension 2 \
---n_train_examples 100 \
+--n_train_examples 50 \
 --batch_size 10 \
 --learning_rate 4e-3 \
 --num_train_epochs 5.0 \
