6b2e9a62609a2b821316d7b0ae77c77f20b14c77
frankaging <zxw0424@gmail.com>
2024-05-29 14:14:26 -0700

    [P1] support ReFT+PEFT by using ReftModel to wrap PeftModel (#46)

--- pyreft/utils.py
+++ pyreft/utils.py
@@ -28,12 +28,13 @@ class TaskType(str, enum.Enum):
     CAUSAL_LM = "CAUSAL_LM"
 
 
-def get_reft_model(model, reft_config, set_device=True):
+def get_reft_model(model, reft_config, set_device=True, disable_model_grads=True):
     """
     Create an instance of ReFT model.
     """
     reft_model = ReftModel(reft_config, model)
     if set_device:
         reft_model.set_device(model.device)
-    reft_model.disable_model_gradients()    
+    if disable_model_grads:
+        reft_model.disable_model_gradients()    
     return reft_model
